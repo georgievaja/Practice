08e8f20cca8c41ef11552dd4b8aab386f9f0e218
Jana Georgieva <georgieva.ja@gmail.com>
2019-07-30 18:35:08 +0200

    sln added

---
+++ Practice.Sharp/Practice.Sharp.Csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice.Sharp.Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
