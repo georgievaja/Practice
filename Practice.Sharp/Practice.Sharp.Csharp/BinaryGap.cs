359b7139539042d040dc19b7737c4464f454e2c9
Jana Georgievová <georgieva.ja@gmail.com>
2019-07-31 21:22:36 +0200

    binary gap C#

--- Practice.Sharp/Practice.Sharp.Csharp/BinaryGap.cs
+++ Practice.Sharp/Practice.Sharp.Csharp/BinaryGap.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 namespace Practice.Sharp.Csharp
 {
-    public class BinaryGap
+    public static class BinaryGap
     {
-        public BinaryGap()
+        public static int GetBinaryGap(string n)
         {
+            if (int.TryParse(n, out int number) && number > 0 && number < 2147483647)
+            {
+                var b = Convert.ToString(number, 2);
+
+                int longest = 0;
+                int curCount = 0;
+
+                for (int i = 0; i < b.Length; i++)
+                {
+                    if (b[i] == '0')
+                    {
+                        curCount++;
+                    }else
+                    {
+                        curCount = 0;
+                    }
+
+                    if (curCount > longest) longest = curCount;
+                }
+
+                return longest;
+            }
+
+            return 0;
         }
     }
 }
--- Practice.Sharp/Practice.Sharp.Csharp/Program.cs
+++ Practice.Sharp/Practice.Sharp.Csharp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static Practice.Sharp.Csharp.BinaryGap;
 
 namespace Practice.Sharp.Csharp
 {
@@ -6,7 +8,13 @@ namespace Practice.Sharp.Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var testData = new string[] { "9", "529", "51272", "15", "53", "2147483647", "2147483648", "0", "-1", "A", null };
+
+            foreach (var data in testData)
+            {
+                var gap = GetBinaryGap(data);
+                Console.WriteLine($"{gap}, {data}");
+            }
         }
     }
 }
