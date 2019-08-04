480f736fd02c34455cc2876c667dbdfdc3ee22ec
Jana Georgievová <georgieva.ja@gmail.com>
2019-08-04 14:51:27 +0200

    binary operators refactoring

--- Practice.Sharp/Practice.Sharp.Fsharp/BinaryGap.fs
+++ Practice.Sharp/Practice.Sharp.Fsharp/BinaryGap.fs
@@ -6,17 +6,8 @@ open Validators
 open Extensions
 open System.Runtime.InteropServices
 
-// lame string method
-let getMaxBinaryGap =
-    evaluateType
-    >=> evaluateRange
-    =>> (Int64.convertToBinary
-            >> String.trimStartOf '0'
-            >> String.trimEndOf '0'
-            >> String.splitBy '1'
-            >> Array.tryMaxBy String.stringLength
-            >> Option.map String.stringLength)
-
+let allOnes (num: int) =
+    ((num+1) &&& num = 0) && (num <> 0)
 
 let rec removeTrailing (num: int) =
     match num &&& 1 = 0 with
@@ -24,14 +15,12 @@ let rec removeTrailing (num: int) =
     | false     ->  num
 
 let rec shift (steps: int) (num: int)= 
-    // find another method to discover all are 1s
-    let before = Convert.ToString (num, 2)     
-    match before.Contains("0") with
-        | false     ->  steps
-        | true      ->  shift (steps + 1) (num ||| (num <<< 1))
+    match allOnes(num) with
+        | true    ->  steps
+        | false   ->  shift (steps + 1) (num ||| (num <<< 1))
 
 
-let getBinaryGap2 =
+let getBinaryGap =
     evaluateType
         >=> evaluateRange
         =>> (removeTrailing
